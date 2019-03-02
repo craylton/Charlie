6f144fcca958746a9d73f34ecb0523e98674b199
Simon Palmer <sipalmer123@gmail.com>
2019-03-02 22:06:59 +0000

    Fix a bug preventing long castles

--- Charlie3/MoveGenerator.cs
+++ Charlie3/MoveGenerator.cs
@@ -225,21 +225,21 @@ namespace Charlie3
             if (board.ToMove == PieceColour.White)
             {
                 // If can short castle
-                if ((board.WhiteCastle & 0x01) != 0 && (board.BitBoard.Occupied & 0x06_00_00_00_00_00_00_00) == 0)
+                if ((board.WhiteCastle & 0b01) != 0 && (board.BitBoard.Occupied & 0x06_00_00_00_00_00_00_00) == 0)
                     moves.Add(new Move(king, 0x02_00_00_00_00_00_00_00, false, true, false, PromotionType.None));
 
                 // If can long castle
-                if ((board.WhiteCastle & 0x10) != 0 && (board.BitBoard.Occupied & 0x70_00_00_00_00_00_00_00) == 0)
+                if ((board.WhiteCastle & 0b10) != 0 && (board.BitBoard.Occupied & 0x70_00_00_00_00_00_00_00) == 0)
                     moves.Add(new Move(king, 0x20_00_00_00_00_00_00_00, false, true, false, PromotionType.None));
             }
             else
             {
                 // If can short castle
-                if ((board.BlackCastle & 0x01) != 0 && (board.BitBoard.Occupied & 0x00_00_00_00_00_00_00_06) == 0)
+                if ((board.BlackCastle & 0b01) != 0 && (board.BitBoard.Occupied & 0x00_00_00_00_00_00_00_06) == 0)
                     moves.Add(new Move(king, 0x00_00_00_00_00_00_00_02, false, true, false, PromotionType.None));
 
                 // If can long castle
-                if ((board.BlackCastle & 0x10) != 0 && (board.BitBoard.Occupied & 0x00_00_00_00_00_00_00_70) == 0)
+                if ((board.BlackCastle & 0b10) != 0 && (board.BitBoard.Occupied & 0x00_00_00_00_00_00_00_70) == 0)
                     moves.Add(new Move(king, 0x00_00_00_00_00_00_00_20, false, true, false, PromotionType.None));
             }
 
