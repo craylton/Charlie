70cf22cb5767eda15bf1d5f9bbd1515fba8f134c
Simon Palmer <sipalmer123@gmail.com>
2019-03-03 21:01:33 +0000

    Add a true alpha beta search function with pruning (which doesnt work)

--- Charlie3/Search.cs
+++ Charlie3/Search.cs
@@ -1,12 +1,11 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Threading.Tasks;
 
 namespace Charlie3
 {
     public class Search
     {
-        private async Task<(Move Move, int Eval)> AlphaBeta(BoardState boardState, int depth)
+        private async Task<(Move Move, int Eval)> MiniMax(BoardState boardState, int depth)
         {
             var evaluator = new Evaluator();
             var generator = new MoveGenerator();
@@ -21,7 +20,7 @@ namespace Charlie3
 
             foreach (var move in moves)
             {
-                var (_, eval) = await AlphaBeta(boardState.MakeMove(move), depth - 1);
+                var (_, eval) = await MiniMax(boardState.MakeMove(move), depth - 1);
 
                 if ((isWhite && eval >= bestEval) || (!isWhite && eval <= bestEval))
                 {
@@ -32,10 +31,45 @@ namespace Charlie3
 
             return (bestMove, bestEval);
         }
+                
+        private async Task<(Move Move, int Eval)> AlphaBeta(BoardState boardState, int alpha, int beta, int depth)
+        {
+            if (depth == 0)
+            {
+                var evaluator = new Evaluator();
+                return (default, evaluator.Evaluate(boardState));
+            }
+
+            var generator = new MoveGenerator();
+            var moves = generator.GenerateLegalMoves(boardState).ToList();
+
+            bool isWhite = boardState.ToMove == PieceColour.White;
+            Move bestMove = default;
+            int bestEval = isWhite ? int.MinValue : int.MaxValue;
+
+            foreach (var move in moves)
+            {
+                var (_, eval) = await AlphaBeta(boardState.MakeMove(move), alpha, beta, depth - 1);
+
+                if ((isWhite && eval >= bestEval) || (!isWhite && eval <= bestEval))
+                {
+                    bestEval = eval;
+                    bestMove = move;
+                }
+
+                if (isWhite && eval > alpha) alpha = eval;
+                if (!isWhite && eval < beta) beta = eval;
+
+                if (alpha >= beta) break;
+            }
+
+            return (bestMove, bestEval);
+        }
 
         public async Task<Move> FindBestMove(BoardState currentBoard)
         {
-            var moveInfo = await AlphaBeta(currentBoard, 3);
+            var moveInfo = await MiniMax(currentBoard, 3);
+            //var moveInfo = await AlphaBeta(currentBoard, int.MinValue, int.MaxValue, 4);
             return moveInfo.Move;
         }
     }
