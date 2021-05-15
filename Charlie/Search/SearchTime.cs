1c05f2c6b5b081ae4130ff1ddc5987926df3fe1b
Simon Palmer <sipalmer123@gmail.com>
2021-05-15 22:52:13 +0100

    Time management changes to avoid flagging in VSTC

--- Charlie/Search/SearchTime.cs
+++ Charlie/Search/SearchTime.cs
@@ -4,11 +4,16 @@ namespace Charlie.Search
 {
     public record SearchTime(int AvailableTime, int Increment)
     {
-        public int MaxTime => AvailableTime / 5;
+        public int MaxTime => Math.Max(AvailableTime / 5, 1);
 
         public bool CanContinueSearching(long elapsedMs, Score eval)
         {
             var denominator = 120 + Math.Clamp((int)eval, -100, 100);
+
+            // Use lower proportion of time when clock is very low
+            if (AvailableTime < 5000)
+                denominator -= (AvailableTime - 5000) / 100;
+
             return elapsedMs <= AvailableTime / denominator + Increment / 4;
         }
     }
