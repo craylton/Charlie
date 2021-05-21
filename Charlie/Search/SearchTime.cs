c05dea7dfcfc5ffffceee330a6bc441c8cce0fef
Simon Palmer <sipalmer123@gmail.com>
2021-05-21 13:54:39 +0100

    Better time management

--- Charlie/Search/SearchTime.cs
+++ Charlie/Search/SearchTime.cs
@@ -4,11 +4,19 @@ namespace Charlie.Search
 {
     public record SearchTime(int AvailableTime, int Increment)
     {
-        public int MaxTime => Math.Max(AvailableTime / 4, 1);
+        public int MaxTime => Math.Max(AvailableTime / 3, 1);
 
         public bool CanContinueSearching(long elapsedMs, Score eval)
         {
-            var denominator = 120 + Math.Clamp((int)eval, -100, 100);
+            // Don't start searching another depth if it's unlikely we'll finish it
+            if (elapsedMs > MaxTime / 3)
+                return false;
+
+            var denominator = 130;
+
+            // Use more time if we aren't doing well
+            if ((int)eval < 50)
+                denominator += Math.Max((int)eval - 50, -100) / 2;
 
             // Use lower proportion of time when clock is very low
             if (AvailableTime < 5000)
